85cd480d9487086c45c46650c71bb40d3b79dc28
Prokopev Vadim <ishvarloiva@gmail.com>
2015-12-06 15:46:44 +0200

    Valuable bug fixes

--- res/js/recipes.js
+++ res/js/recipes.js
@@ -9,7 +9,11 @@ function showCategory(category)
 			$("#content_js").empty();
 			for(id in data)
 			{
-				$("#content_js").append(recipes.small({id: id, name:data[id].name, image:data[id].image, description:data[id].description, recipe:data[id].recipe});
+				$("#content_js").append(recipes.small({id: id,
+					name:data[id].name,
+					image:data[id].image,
+					description:data[id].description,
+					recipe:data[id].recipe});
 			}
 		}
 		dataType: "json"
