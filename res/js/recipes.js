6614d01da2472545f853c99ba67474e8f93b8fe2
Prokopev Vadim <ishvarloiva@gmail.com>
2015-12-07 18:40:56 +0200

    Template fixes

--- res/js/recipes.js
+++ res/js/recipes.js
@@ -9,7 +9,7 @@ function showCategory(category)
 			$("#content_js").empty();
 			for(id in data)
 			{
-				$("#content_js").append(recipes.small({id: id,
+				$("#content_js").append(recipes.small({
 					name:data[id].name,
 					image:data[id].image,
 					description:data[id].description,
