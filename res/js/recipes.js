9700a625b1fa3aa01e8ab59783651b6c2c78fc2f
Prokopev Vadim <ishvarloiva@gmail.com>
2015-12-15 21:29:14 +0200

    I HOPE DAT IZ FIKS

--- res/js/recipes.js
+++ res/js/recipes.js
@@ -8,44 +8,21 @@ function showCategory(category)
 		{
 			$("#content_js").empty();
 			$("#content_js").append(recipes.navigation);
-			var column = "column0";
-			var div = "<div class='grid_4'>";
-			for(id in column)
-			{
-				div += recipes.small({
-					id:id,
-					name:id.name,
-					image:id.image,
-					description:id.description});
-			}
-			div += "</div>";
-			$("#content_js").append(div);
 			
-			var column = "column1";
-			var div = "<div class='grid_4'>";
-			for(id in column)
+			for(column in data)
 			{
-				div += recipes.small({
-					id:id,
-					name:id.name,
-					image:id.image,
-					description:id.description});
+				var div = "<div class='grid_4'>";
+				for(id in data[column])
+				{
+					div += recipes.small({
+						id:id,
+						name:id.name,
+						image:id.image,
+						description:id.description});
+				}
+				div += "</div>";
+				$("#content_js").append(div);
 			}
-			div += "</div>";
-			$("#content_js").append(div);
-			
-			var column = "column2";
-			var div = "<div class='grid_4'>";
-			for(id in column)
-			{
-				div += recipes.small({
-					id:id,
-					name:id.name,
-					image:id.image,
-					description:id.description});
-			}
-			div += "</div>";
-			$("#content_js").append(div);
 		},
 		dataType: "json"
 	});
