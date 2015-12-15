23a825473788c4eaec6f266c2b6042efda72553d
Prokopev Vadim <ishvarloiva@gmail.com>
2015-12-15 17:12:23 +0200

    Recipes now in better borders

--- res/js/recipesTemplates.js
+++ res/js/recipesTemplates.js
@@ -17,7 +17,7 @@ if (goog.DEBUG) {
 
 
 recipes.small = function(opt_data, opt_ignored) {
-  return '<a href="javascript:showRecipe(\'' + soy.$$escapeHtml(opt_data.name) + '\', \'' + soy.$$escapeHtml(opt_data.image) + '\', \'' + soy.$$escapeHtml(opt_data.id) + '\')"><div class="grid_4"><fieldset><image src="' + soy.$$escapeHtml(opt_data.image) + '" height="250px" width="250px"><p class="recipe_name">' + soy.$$escapeHtml(opt_data.name) + '</p><p class="recipe_desc">' + soy.$$escapeHtml(opt_data.description) + '</p></fieldset></div></a>';
+  return '<a href="javascript:showRecipe(\'' + soy.$$escapeHtml(opt_data.name) + '\', \'' + soy.$$escapeHtml(opt_data.image) + '\', \'' + soy.$$escapeHtml(opt_data.id) + '\')"><div class="grid_4"><div class="recipe"><image src="' + soy.$$escapeHtml(opt_data.image) + '" height="250px" width="250px"><h3>' + soy.$$escapeHtml(opt_data.name) + '</h3><p class="recipe_desc">' + soy.$$escapeHtml(opt_data.description) + '</p></div></div></a>';
 };
 if (goog.DEBUG) {
   recipes.small.soyTemplateName = 'recipes.small';
@@ -25,8 +25,16 @@ if (goog.DEBUG) {
 
 
 recipes.recipe = function(opt_data, opt_ignored) {
-  return '<div class="grid_12"><image src="' + soy.$$escapeHtml(opt_data.image) + '" height="400px" width="400px"><p class="recipe_name">' + soy.$$escapeHtml(opt_data.name) + '</p><p class="recipe_text">' + soy.$$escapeHtml(opt_data.recipe) + '</p></div>';
+  return '<div class="grid_12"><image src="' + soy.$$escapeHtml(opt_data.image) + '" height="400px" width="400px"><h3>' + soy.$$escapeHtml(opt_data.name) + '</h3><p class="recipe_text">' + soy.$$escapeHtml(opt_data.recipe) + '</p></div>';
 };
 if (goog.DEBUG) {
   recipes.recipe.soyTemplateName = 'recipes.recipe';
+}
+
+
+recipes.navigation = function(opt_data, opt_ignored) {
+  return '<div class="grid_12"><a href="javascript:showCategory(\'appetizer\');"><div class="grid_2 category alpha"><h3>Appetizers</h3></div></a><a href="javascript:showCategory(\'main\');"><div class="grid_2 category"><h3>Main</h3></div></a><a href="javascript:showCategory(\'side\');"><div class="grid_2 category"><h3>Side</h3></div></a><a href="javascript:showCategory(\'drink\');"><div class="grid_2 category"><h3>Drinks</h3></div></a><a href="javascript:showCategory(\'dessert\');"><div class="grid_2 category"><h3>Desserts</h3></div></a><a href="javascript:showCategory(\'all\');"><div class="grid_2 category omega"><h3>All</h3></div></a></div>';
+};
+if (goog.DEBUG) {
+  recipes.navigation.soyTemplateName = 'recipes.navigation';
 }
